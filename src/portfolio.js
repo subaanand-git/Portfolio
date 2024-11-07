80deb63615773f1cd035d181cbe2865327582e73
subaanand-git <subaanand1070@gmail.com>
2024-11-07 18:26:30 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,540 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Suba Anand",
+  logo_name: "Suba Anand()",
+  nickname: "Suba Anand",
+  full_name: "Suba Anand",
+  subTitle:
+    "Full Stack Developer, Software Learning Enthusiast. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1kcvW8P65jWZFeKR6Jopf3nrQLA1qJCwn/view?usp=sharing",
+  mail: "subaanand1070@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Social Media Link */
+  github: "https://github.com/subaanand-git",
+  linkedin: "https://www.linkedin.com/in/suba-anand-430302227/",
+  gmail: "subaanand1070@gmail.com",
+  // gitlab: " ",
+  // facebook: " ",
+  // twitter: " ",
+  // instagram: " ",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS and Bootstrap",
+        "⚡ Developing web applications using MERN STACK",
+        "⚡ Backend development using NodeJS, ExpressJS and Mongodb",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        // {
+        //   skillName: "ExpressJS",
+        //   fontAwesomeClassname: "simple-icons:expressdotjs",
+        //   style: {
+        //     color: "white",
+        //   },
+        // },
+        {
+          skillName: "Tailwind CSS",
+          fontAwesomeClassname: "simple-icons:tailwindcss",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:nodedotjs",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        {
+          skillName: "Vercel",
+          fontAwesomeClassname: "simple-icons:vercel",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Render",
+          fontAwesomeClassname: "simple-icons:render",
+          style: {
+            color: "grey",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Git-Hub",
+          fontAwesomeClassname: "simple-icons:github",
+          style: {
+            color: "grey",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    },
+
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing web apps",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "vercel",
+          fontAwesomeClassname: "simple-icons:vercel",
+          style: {
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Render",
+          fontAwesomeClassname: "simple-icons:render",
+          style: {
+            color: "grey",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "GitHub",
+          fontAwesomeClassname: "simple-icons:github",
+          style: {
+            color: "grey",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Education and Certifications page
+const degrees = {
+  degrees: [
+    {
+      title: "Anna University",
+      subtitle: "B.E. in Electronics and Communication Engineering",
+      logo_path: "dal.png",
+      alt_name: "SETI",
+      duration: "2015 - 2019",
+      descriptions: [
+        "⚡ I have studied core subjects like Electronic Devices, Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ Studied Advanced Software Development Concepts, Data Management",
+        "⚡ I have also completed various online courses for Web Development and App Development etc.",
+        "⚡ I have implemented several projects based on what I've learnt under my Electronics Engineering course. ",
+      ],
+      website_link: "https://www.annauniv.edu/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Entri Certified Full Stack Developer",
+      subtitle: "2023-2024",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        " ",
+      alt_name: "deeplearning.ai",
+      color_code: "#47A048",
+    },
+    {
+      title: "Tiliconveli Certified FrontEnd Developer",
+      subtitle: "2023-2024",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        " ",
+      alt_name: "deeplearning.ai",
+      color_code: "#F6B808",
+    },
+    {
+      title: "MERN STACK Developer",
+      subtitle: "2024",
+      logo_path: "stanford_logo.png",
+      certificate_link:
+        " ",
+      alt_name: "Stanford University",
+      color_code: "#8C151599",
+    }
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work and Internship",
+  description:
+    "I've completed internships. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Full Stack Developer",
+          company: "Entri Elevate",
+          company_url: "https://entri.app/",
+          logo_path: "buld.jpg",
+          duration: "Mar 2024 - Ongoing",
+          location: "Bangalore",
+          description:
+            "Develop a website using MERN STACK Technologies.",
+          color: "#f10000",
+        },
+        {
+          title: "Planning Engineer",
+          company: "Uniglaze India Pvt.Ltd",
+          company_url: "https://www.uniglaze.in/ ",
+          logo_path: "quicko.jpg",
+          duration: "October 2019 - january 2024",
+          location: "Bangalore",
+          description:
+            "Uniglaze is the india's most Trusted, Innovative and Technologically Advanced, building Facade, Windows & Doors Company.",
+          color: "#2962FF",
+        },
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Full-Stack Developer",
+          company: "Entri",
+          company_url: "https://entri.app/",
+          logo_path: "buld.jpg",
+          duration: "March 2024 - October 2024",
+          location: "Work From Home",
+          description: "Develop a website using MERN STACK Technologies.",
+          color: "#56A4D3",
+        },
+        {
+          title: "Front-End Developer",
+          company: "Tiliconveli Tech Park",
+          company_url: "https://www.tiliconveli.com/",
+          logo_path: "vjt.png",
+          duration: "February 2024",
+          location: "Tirunelveli",
+          description: "Develop a portfolio website using ReactJS",
+          color: "#800000",
+        },
+      ],
+    }
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create MERN projects. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "Anand.jpeg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try my best to write a blog about it.",
+    // link: "https://medium.com/@hrishipatel99",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Crackers Online Shopping",
+      url: "https://github.com/subaanand-git/MERN-Ecommerce-website.git",
+      description: "E-commerce website made with ReactJS, MongoDB,Nodejs, Expressjs.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "devicon-bootstrap",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "Express.js",
+          iconifyClass: "devicon-express",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      name: "Signup Form",
+      url: "https://github.com/subaanand-git/reacthook.git",
+      description:
+        "Signup Form made with ReactJS(React Hook Form) and Bootstrap Framework.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "devicon-bootstrap",
+        },
+      ],
+    },
+    {
+      name: "Portfolio",
+      url: " ",
+      description: "My personal Portfolio website made with ReactJS,HTML ,CSS, Javascript and Bootstrap.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "devicon-html5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "devicon-css3",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "devicon-bootstrap",
+        },
+      ],
+    },
+    {
+      name: "Analog Clock",
+      url: "https://github.com/subaanand-git/script5.git",
+      description: "Analog Clock  made with HTML ,CSS, Javascript.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "devicon-html5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "devicon-css3",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      name: "Furniture Online Shopping",
+      url: "https://github.com/subaanand-git/Bootstrap-Assignment.git",
+      description:
+        "E-commerce website made with HTML5, CSS3, Javascript, ReactJS and Bottstrap.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "devicon-html5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "devicon-css3",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "devicon-bootstrap",
+        },
+      ],
+    },
+    {
+      name: "Login Form",
+      url: "https://github.com/subaanand-git/express1.git",
+      description:
+        "Login Form made with ExpressJS and validate the email and password using JWT auth.",
+      languages: [
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "Express.js",
+          iconifyClass: "devicon-express",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      name: "Calculator App",
+      url: "https://github.com/subaanand-git/expressjs.git",
+      description:
+        "A Calculator webapp made with ExpressJS and Postman endpoints.",
+      languages: [
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "Express.js",
+          iconifyClass: "devicon-express",
+        },
+        {
+          name: "Postman",
+          iconifyClass: "devicon-postman",
+        },
+      ],
+    },
+    {
+      name: "Employee Management System",
+      url: " ",
+      description:
+        "Employee management system made with ReactJS,Nodejs, Expressjs,MongoDB and implement CRUD operations.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "Express.js",
+          iconifyClass: "devicon-express",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
